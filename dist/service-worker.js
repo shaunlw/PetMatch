ff5227fb448ca3693ee2f30801ebb34158812cbd
Xiangli Wang <xlw@Macintosh-42.local>
2016-12-15 09:06:51 -0800

    grunt

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -8,7 +8,7 @@ var urlsToCache = [
   '//yoav-zibin.github.io/TicTacToe/dist/js/everything.min.js',
   '//yoav-zibin.github.io/TicTacToe/dist/css/everything.min.css',
 ];
-var CACHE_NAME = 'cache-v2016-12-14T19:27:46.053Z';
+var CACHE_NAME = 'cache-v2016-12-15T17:06:07.528Z';
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
